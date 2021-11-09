67956807144666d10d962a1033064db157b4b185
YuriPierriV <91505118+YuriPierriV@users.noreply.github.com>
2021-11-09 00:37:30 -0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,4 +4,85 @@
 #include <stdio.h> 
 #include <string.h>
 
+void logo(){
+	
+	
+};
 
+void tabelaInicial(){
+	printf("\t\t _____________________________\n");
+	printf("\t\t|       Sistema de dados      |\n");
+	printf("\t\t|                             |\n");
+	printf("\t\t|   1  ->    Clientes         |\n");
+	printf("\t\t|                             |\n");
+	printf("\t\t|   2  ->    Produtos         |\n");
+	printf("\t\t|                             |\n");
+	printf("\t\t|   3  ->  Compra/Vendas      |\n");
+	printf("\t\t|                             |\n");
+	printf("\t\t|                             |\n");
+	printf("\t\t###############################");
+	
+};
+
+void subTabelaClientes(){
+	printf("\t\t _____________________________\n");
+	printf("\t\t|          Clientes           |\n");
+	printf("\t\t|                             |\n");
+	printf("\t\t|   1  ->    Cadastrar        |\n");
+	printf("\t\t|                             |\n");
+	printf("\t\t|   2  ->    Listar           |\n");
+	printf("\t\t|                             |\n");
+	printf("\t\t|   3  ->    Atualizar        |\n");
+	printf("\t\t|                             |\n");
+	printf("\t\t|   4  ->    Deletar          |\n");
+	printf("\t\t|                             |\n");
+	printf("\t\t###############################");
+	
+	
+};
+
+void subTabelaProdutos(){
+	printf("\t\t _____________________________\n");
+	printf("\t\t|          Produtos           |\n");
+	printf("\t\t|                             |\n");
+	printf("\t\t|   1  ->    Cadastrar        |\n");
+	printf("\t\t|                             |\n");
+	printf("\t\t|   2  ->    Listar           |\n");
+	printf("\t\t|                             |\n");
+	printf("\t\t|   3  ->    Atualizar        |\n");
+	printf("\t\t|                             |\n");
+	printf("\t\t|   4  ->    Deletar          |\n");
+	printf("\t\t|                             |\n");
+	printf("\t\t###############################");
+	
+	
+};
+
+void subTabelaCp(){
+	printf("\t\t _____________________________\n");
+	printf("\t\t|       Compras/vendas        |\n");
+	printf("\t\t|                             |\n");
+	printf("\t\t|   1  ->    Cadastrar        |\n");
+	printf("\t\t|                             |\n");
+	printf("\t\t|   2  ->    Listar           |\n");
+	printf("\t\t|                             |\n");
+	printf("\t\t|   3  ->    Atualizar        |\n");
+	printf("\t\t|                             |\n");
+	printf("\t\t|   4  ->    Deletar          |\n");
+	printf("\t\t|                             |\n");
+	printf("\t\t###############################");
+	
+	
+};
+
+int main(){
+	tabelaInicial();
+	printf("\n\n");
+	subTabelaClientes();
+	printf("\n\n");
+	subTabelaProdutos();
+	printf("\n\n");
+	subTabelaCp();
+	
+	
+}
